574536f4894f9833cae2dddd59b37998a39289f3
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-01 18:48:29 -0600

    Add files via upload

---
+++ indentation-python.py
@@ -0,0 +1,23 @@
+# another example
+is_old = True
+is_licenced = True
+
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# Be CAREFUL of spacing and indentation in python another example
+is_old = True
+is_licenced = True
+
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+    
+    print('okoko')
